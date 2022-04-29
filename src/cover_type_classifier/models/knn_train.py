e969ae63a498f2641cfdb87855c5b381e6d5bb23
Victoria Lebedeva <vict1464@gmail.com>
2022-04-29 06:58:59 +0300

    feat: add knn save predictions

--- src/cover_type_classifier/models/knn_train.py
+++ src/cover_type_classifier/models/knn_train.py
@@ -1,17 +1,10 @@
 import click 
+import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from cover_type_classifier.data import get_dataset 
 
 
 @click.command()
-@click.option(
-    '-d',
-    '--dataset-path',
-    default='data/external/train.csv',
-    type=click.Path(exists=True, dir_okay=False),
-    show_default=True,
-)
-
 @click.option(
     '-d',
     '--dataset-path',
@@ -39,7 +32,13 @@ def train(dataset_path, random_state, test_size):
 
     knn = KNeighborsClassifier()
     knn.fit(X_train, y_train)
-    knn.predict(X_val)
+    y_pred = knn.predict(X_val)
+  
+
+    df = pd.DataFrame(X_val.index, columns=['Id'])
+    df['Cover_Type'] = y_pred
+
+    df.to_csv('models/knn.csv', index=False)  
 
 if __name__ == '__main__':
     train()
