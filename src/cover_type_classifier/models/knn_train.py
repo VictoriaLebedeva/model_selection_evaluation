7342637fa979935351014d66aa1dc8d9302d91a3
Victoria Lebedeva <43277516+VictoriaLebedeva@users.noreply.github.com>
2022-05-04 09:55:06 +0300

    feat: mlflow tracking experiment

--- src/cover_type_classifier/models/knn_train.py
+++ src/cover_type_classifier/models/knn_train.py
@@ -1,12 +1,15 @@
 import click
 import os
 import pandas as pd
+import numpy as np
+import mlflow
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.utils import shuffle
 from cover_type_classifier.data import get_dataset
 from datetime import datetime
 
+mlflow_experiment_id = 1
 
 @click.command()
 @click.option(
@@ -68,20 +71,26 @@ def train(
         X_train, y_train, random_state=42
     )
     # train model and make a prediction
-    knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
-    print("Estimator", knn)
-    knn.fit(X_train, y_train)
+    with mlflow.start_run(experiment_id=mlflow_experiment_id):
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+        print("Estimator", knn)
+        knn.fit(X_train, y_train)
 
-    # cross-validation
-    metrics = ["balanced_accuracy", "f1_weighted", "roc_auc_ovo"]
-    print("Cross-Validation score results")
-    for metric in metrics:
-        print(
-            f"{metric}:",
-            cross_val_score(
-                knn, X_train_shuffled, y_train_shuffled, cv=5, scoring=metric
-            ),
-        )
+        # cross-validation
+        metrics = ["balanced_accuracy", "f1_weighted", "roc_auc_ovo"]
+        print("Cross-Validation score results")
+
+        metrics_scores = {} 
+
+        for metric in metrics:
+            scores = cross_val_score(knn, X_train_shuffled, y_train_shuffled, cv=5, scoring=metric)
+            metrics_scores[metric] = np.mean(scores)
+            print(f"{metric}:", scores)
+
+        mlflow.log_param('n_neighbors', n_neighbors)
+        mlflow.log_param('weights', weights)
+        mlflow.log_metric('f1_weighted', metrics_scores['f1_weighted'])
+        mlflow.sklearn.log_model(knn, 'model')
 
     y_pred = knn.predict(X_test)
 
